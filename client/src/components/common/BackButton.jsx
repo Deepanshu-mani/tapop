f1f117d54c63a577b29ffcbc5f061615db19e843
Mani <deepanshukumar1542004@gmail.com>
2024-12-03 14:25:39 +0530

    update files

--- client/src/components/common/BackButton.jsx
+++ client/src/components/common/BackButton.jsx
@@ -10,7 +10,7 @@ const BackButton = () => {
         className="flex items-center space-x-2 border-2 rounded-full px-3 py-3 text-gray-600 hover:text-gray-800 bg-gray-100 hover:bg-gray-200 hover:scale-95 transition-all duration-200 ease-in-out"
       >
         <FaArrowLeft className="w-6 h-6" />
-        <span className="font-bold text-3xl">XT-MANI</span>
+        <span className="font-bold text-3xl">BACK</span>
       </Link>
     </div>
   );
