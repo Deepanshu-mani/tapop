1bc53a7136ae48c478c5dc091f00895a21de36a6
Mani <deepanshukumar1542004@gmail.com>
2024-12-03 14:47:01 +0530

    update files

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,14 +3,14 @@ import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, si
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCxDypwrCx5cVNp-m_q9jmIr6vnqAROi64",
-    authDomain: "tapop-112211.firebaseapp.com",
-    projectId: "tapop-112211",
-    storageBucket: "tapop-112211.appspot.com", 
-    messagingSenderId: "622760826795",
-    appId: "1:622760826795:web:a0422a306159de545a64ee",
-    measurementId: "G-RZ0KYJC8FH"
-};
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
